e7e338f0a282c7601430e323c4c92e53c3310b33
luqmansen <luqmansen@gmail.com>
2019-04-12 22:38:13 +0700

    added config file in setting

--- bola.py
+++ bola.py
@@ -7,164 +7,120 @@ Created on Sun Feb 17 17:51:29 2019
 import cv2 as cv
 import numpy as np
 import imutils
-import time
+import rw_file as rw
 
-def saveConfig(value, file_name):
-    value = str(value)
-    filename = file_name + ".txt"
-    file = open(str(filename), "w")
-    file.write(value)
-    file.close()
-
-def read(file):
-    f = open(file, "r")
-    return f.read()  
 
 cap = cv.VideoCapture(0)
-#cap.set(cv.CAP_PROP_FRAME_WIDTH, 120)
-#cap.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 120)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 240)
 
 cv.namedWindow("trackbars", cv.WINDOW_NORMAL)
 cv.resizeWindow("trackbars", 300, 500)
-cv.createTrackbar("L - H", "trackbars", int(read("setting/LH.txt")), 179, lambda x: saveConfig(x, "setting/LH"))
-cv.createTrackbar("L - S", "trackbars", int(read("setting/LS.txt")), 255, lambda x : saveConfig(x, "setting/LS"))
-cv.createTrackbar("L - V", "trackbars", int(read("setting/LV.txt")), 255, lambda x : saveConfig(x, "setting/LV"))
-cv.createTrackbar("U - H", "trackbars", int(read("setting/UH.txt")), 179, lambda x: saveConfig(x, "setting/UH"))
-cv.createTrackbar("U - S", "trackbars", int(read("setting/US.txt")), 255, lambda x: saveConfig(x, "setting/US"))
-cv.createTrackbar("U - V", "trackbars", int(read("setting/UV.txt")), 255, lambda x : saveConfig(x, "setting/UV"))
-#Trackbar untuk dilation, erosion, gausian
-cv.createTrackbar("dilation", "trackbars", int(read("setting/dilation.txt")), 100, lambda x : saveConfig(x, "setting/dilation"))	
-cv.createTrackbar("DIL iterations", "trackbars", int(read("setting/dilation_iterations.txt")), 200, lambda x : saveConfig(x, "setting/dilation_iterations"))
-cv.createTrackbar("erosion", "trackbars", int(read("setting/erosion.txt")), 100, lambda x : saveConfig(x, "setting/erosion"))
-cv.createTrackbar("ER iterations", "trackbars", int(read("setting/erosion_iterations.txt")), 200, lambda x : saveConfig(x, "setting/erosion_iterations"))
-cv.createTrackbar("gaussian", "trackbars", int(read("setting/gaussian.txt")), 200, lambda x : saveConfig(x, "setting/gaussian"))
+cv.createTrackbar("L - H", "trackbars", int(rw.read("setting/LH.txt")), 179, lambda x: rw.write(x, "setting/LH.txt"))
+cv.createTrackbar("L - S", "trackbars", int(rw.read("setting/LS.txt")), 255, lambda x : rw.write(x, "setting/LS.txt"))
+cv.createTrackbar("L - V", "trackbars", int(rw.read("setting/LV.txt")), 255, lambda x : rw.write(x, "setting/LV.txt"))
+cv.createTrackbar("U - H", "trackbars", int(rw.read("setting/UH.txt")), 179, lambda x: rw.write(x, "setting/UH.txt"))
+cv.createTrackbar("U - S", "trackbars", int(rw.read("setting/US.txt")), 255, lambda x: rw.write(x, "setting/US.txt"))
+cv.createTrackbar("U - V", "trackbars", int(rw.read("setting/UV.txt")), 255, lambda x : rw.write(x, "setting/UV.txt"))
 
-#OPENING
-#cv.createTrackbar("opening\r\n kernel\r\n size", "trackbars", int(read("setting/opening.txt")), 100, lambda x : saveConfig(x, "setting/opening"))
-#cv.createTrackbar("kernel\r\n type", "trackbars", int(read("setting/opening_kernel_type.txt")), 2, lambda x : saveConfig(x, "setting/opening_kernel_type"))
-#cv.createTrackbar("opening\r\n iterations", "trackbars", int(read("setting/opening_iterations.txt")), 200, lambda x : saveConfig(x, "setting/opening_iterations"))
-#cv.createTrackbar("gaussian", "trackbars", int(read("setting/gaussian.txt")), 200, lambda x : saveConfig(x, "setting/gaussian"))
-
-#trackbar untuk setting radius di bola
-cv.createTrackbar("radius", "trackbars", int(read("setting/radius.txt")), 200, lambda x : saveConfig(x, "setting/radius"))
+cv.createTrackbar("dilation", "trackbars", int(rw.read("setting/dilation_bola.txt")), 100, lambda x : rw.write(x, "setting/dilation_bola.txt"))
+cv.createTrackbar("Dilation iterations", "trackbars", int(rw.read("setting/dilation_iteration_bola.txt")), 200, lambda x : rw.write(x, "setting/dilation_iteration_bola.txt"))
+cv.createTrackbar("erosion", "trackbars", int(rw.read("setting/erosion_bola.txt")), 100, lambda x : rw.write(x, "setting/erosion_bola.txt"))
+cv.createTrackbar("Erosion iterations", "trackbars", int(rw.read("setting/erosion_iteration_bola.txt")), 200, lambda x : rw.write(x, "setting/erosion_iteration_bola.txt"))
+cv.createTrackbar("gaussian", "trackbars", int(rw.read("setting/gaussian_bola.txt")), 200, lambda x : rw.write(x, "setting/gaussian_bola.txt"))
+cv.createTrackbar("radius", "trackbars", int(rw.read("setting/radius_bola.txt")), 200, lambda x : rw.write(x, "setting/radius_bola.txt"))
 
 while True:
 #    start = time.time()
     ret, frame = cap.read()
     frame =  imutils.resize(frame, width=300)
-    
+
     tinggi, panjang, _ = frame.shape
-    
-    gaussian_kernel = int(read("setting/gaussian.txt"))
+
+    gaussian_kernel = int(rw.read("setting/gaussian_bola.txt"))
     if gaussian_kernel == 0:
         gaussian_kernel = 1
     else:
         gaussian_kernel = (2*gaussian_kernel)+1
-    
+
     frame = cv.GaussianBlur(frame, (gaussian_kernel,gaussian_kernel), 0)
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
- 
-    l_h = int(read("setting/LH.txt"))
-    l_s = int(read("setting/LS.txt"))
-    l_v = int(read("setting/LV.txt"))
-    u_h = int(read("setting/UH.txt"))
-    u_s = int(read("setting/US.txt"))
-    u_v = int(read("setting/UV.txt"))
-    
-    l_h = int(read("setting/LH.txt"))
-    l_s = int(read("setting/LS.txt"))
-    l_v = int(read("setting/LV.txt"))
-    u_h = int(read("setting/UH.txt"))
-    u_s = int(read("setting/US.txt"))
-    u_v = int(read("setting/UV.txt"))
-    
-    
-    lower_white = np.array([l_h,l_s,l_v])   
+
+    l_h = int(rw.read("setting/LH.txt"))
+    l_s = int(rw.read("setting/LS.txt"))
+    l_v = int(rw.read("setting/LV.txt"))
+    u_h = int(rw.read("setting/UH.txt"))
+    u_s = int(rw.read("setting/US.txt"))
+    u_v = int(rw.read("setting/UV.txt"))
+
+    l_h = int(rw.read("setting/LH.txt"))
+    l_s = int(rw.read("setting/LS.txt"))
+    l_v = int(rw.read("setting/LV.txt"))
+    u_h = int(rw.read("setting/UH.txt"))
+    u_s = int(rw.read("setting/US.txt"))
+    u_v = int(rw.read("setting/UV.txt"))
+
+
+    lower_white = np.array([l_h,l_s,l_v])
     upper_white = np.array([u_h,u_s,u_v])
-    
+
     mask = cv.inRange(hsv, lower_white, upper_white)
-    
-    ################# kalo mau pake opening
-#    opening_kernel_size = int(read("setting/opening.txt"))
-#    if opening_kernel_size == 0:
-#        opening_kernel_size = 1
-#    else:
-#        opening_kernel_size = (2*opening_kernel_size)+1
-#        
-#    opening_iterations = int(read("setting/opening_iterations.txt"))
-#    
-#    kernel_type = int(read("setting/opening_kernel_type.txt"))
-#    if kernel_type == 0:
-#        kernel_type = cv.MORPH_RECT
-#    elif kernel_type == 1:
-#        kernel_type = cv.MORPH_ELLIPSE
-#    elif kernel_type == 2:
-#        kernel_type = cv.MORPH_CROSS
-#    
-#    mask = cv.morphologyEx(mask, cv.MORPH_OPEN, cv.getStructuringElement(kernel_type,(opening_kernel_size, opening_kernel_size )), iterations = opening_iterations)
-    
-    erosion = int(read("setting/erosion.txt"))
+
+    erosion = int(rw.read("setting/erosion_bola.txt"))
     if erosion == 0:
-        erosion = 1 
+        erosion = 1
     else:
         erosion = (2*erosion)+1
-        
-    dilation= int(read("setting/dilation.txt"))
+
+    dilation= int(rw.read("setting/dilation_bola.txt"))
     if dilation == 0:
         dilation = 1
     else:
         dilation = (2*dilation)+1
 
-    erosion_iterations = int(read("setting/erosion_iterations.txt"))
-    dilation_iterations = int(read("setting/dilation_iterations.txt"))
-    
+    erosion_iterations = int(rw.read("setting/erosion_iteration_bola.txt"))
+    dilation_iterations = int(rw.read("setting/dilation_iteration_bola.txt"))
+
     erosion_kernel = cv.getStructuringElement(cv.MORPH_RECT, (erosion, erosion))
     dilation_kernel = cv.getStructuringElement(cv.MORPH_RECT, (dilation, dilation))
-    
+
     mask = cv.erode(mask, erosion_kernel, iterations = erosion_iterations)
     mask = cv.dilate(mask, dilation_kernel, iterations = dilation_iterations)
-    
+
     result = cv.bitwise_and(frame, frame, mask = mask)
 
-    _, contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    
-    center = 0
+    contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    x = 0
+    y = 0
+    radius = 0
+    center = None
+
     if len(contours) > 0:
-        c = max(contours, key= cv.contourArea)
-        ((x,y), radius) = cv.minEnclosingCircle(c)
+        c = max(contours, key=cv.contourArea)
+        ((x, y), radius) = cv.minEnclosingCircle(c)
         M = cv.moments(c)
-        
-        # untuk calculate centroid
-        if int(M["m00"])> 0:
+        cx = 0
+        cy = 0
+
+        if int(M["m00"]) > 0:
             cx = int(M["m10"]) / int(M["m00"])
             cy = int(M["m01"]) / int(M["m00"])
-            center = (int(cx), int(cy))        
+            center = (int(cx), int(cy))
         else:
-            center = (1,1)
-            
-        rads = int(read("setting/radius.txt"))
-        if radius > rads :
-            
-            if x < panjang/3 and y < 2*tinggi/3:
-                cv.putText(result, "KIRI ATAS", (10, tinggi - 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (100,255,10), 1)
-            elif x < 2*panjang/3 and y < 2*tinggi/3:
-                cv.putText(result, "TENGAH ATAS", (10, tinggi - 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (100,250,10),1)
-            elif x > 2*panjang/3 and y < 2*tinggi/3:
-                cv.putText(result, "KANAN ATAS", (10, tinggi - 40), cv.FONT_HERSHEY_SIMPLEX, 0.5, (100,250,10),1)
-            
-            cv.circle(result, (int(x), int(y)), int(radius), (0,255,255), 2)
-            cv.circle(result, center, 5, (0,0,255), -1)
-            cv.putText(result, "x : {} y : {}".format(int(cx), int(cy)), (10, tinggi-25), cv.FONT_HERSHEY_COMPLEX_SMALL,0.8, (10,255,10))
-            
-#    end = time.time()
-#    fps = str(int(1/(end-start)))
-#    cv.putText(result, fps, (10, tinggi-55), cv.FONT_HERSHEY_COMPLEX_SMALL,0.8, (10,255,10))
-#   Buat Garis Area di Layar
+            center = None
+
+    rads = int(rw.read("setting/radius_bola.txt"))
+    if radius > rads :
+
+        cv.circle(result, (int(x), int(y)), int(radius), (0,255,255), 2)
+        cv.circle(result, center, 5, (0,0,255), -1)
+        cv.putText(result, "x : {} y : {}".format(int(cx), int(cy)), (10, tinggi-25), cv.FONT_HERSHEY_COMPLEX_SMALL,0.8, (10,255,10))
+
     cv.line(result, (int(panjang/3), tinggi), (int(panjang/3),0), (0,255,0), 2) #kiri
     cv.line(result, (int(2*panjang/3), tinggi), (int(2*panjang/3),0), (0,255,0), 2) # kanan
     cv.line(result, (0, int(2*tinggi/3)), (panjang, int(2*tinggi/3) ), (123,10,32), 2) #bawah
-    
-    
+
     cv.imshow("result", result)
     cv.imshow("mask", mask)
     cv.imshow("frame", frame)
